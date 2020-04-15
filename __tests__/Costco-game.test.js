592a3d6ff3c76e7db930174cc0562bca8f90dc64
Julia Seidman <juliaseid@gmail.com>
2020-04-15 10:21:11 -0700

    construct Character class and begin creating tests

---
+++ __tests__/Costco-game.test.js
@@ -0,0 +1,1 @@
+import { Character } from "./../src/characters.js";
---
+++ src/characters.js
@@ -0,0 +1,49 @@
+export class Character {
+  constructor(name, seniority, stressLevel) {
+    this.name = name;
+    this.seniority = seniority;
+    this.stressLevel = stressLevel;
+    this.timeOff = 0;
+
+  }
+//levelUp() {
+//  
+//}
+//
+//stressLevel() {
+//  
+//}
+//
+//timeOff() {
+//  
+//}
+//
+//checkForWin() {
+//  
+//}
+}
+
+//class PizzaCutter extends Character {
+//
+//}
+
+//class ChickenCooker extends Character {
+//  
+//}
+
+//class ProduceStocker extends Character {
+//  
+//}
+
+//class DoorPerson extends Character {
+//  
+//}
+
+//class Supervisor extends Character {
+//  
+//}
+
+//class Manager extends Character {
+//  
+//}
+
