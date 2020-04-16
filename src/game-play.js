80debfb99274040266d609446a6cec0c6d6ca0e7
Julia Seidman <juliaseid@gmail.com>
2020-04-16 11:50:54 -0700

    Write basic battle functionality

--- src/game-play.js
+++ src/game-play.js
@@ -7,7 +7,6 @@ import { Supervisor } from './../src/characters.js';
 import { Manager } from './../src/characters.js';
 import { StoreManager } from './../src/characters.js';
 
-
 export class Game {
   constructor(){
     this.characters = [];
@@ -53,35 +52,67 @@ export class Game {
     this.characters.push(new StoreManager("Juanita", "storeManager"));
   }
 
-
   chooseOpponent(){
     let ranNumber = Math.floor(Math.random() * characters.length) + 1;
-    return ranNumber;
-  }
+    let opponent = this.characters[ranNumber];
+    return opponent;
+  };
 
-  diceRoll(){
-    let dicePair = [];
-    for (i = 0; i < 2; i++) {
-      let ranNumber = Math.floor(Math.random() * 6) + 1;
-      dicePair.push(diceNumber);
-    }
-    return dicePair;
-  }
-
-  battle(){
-    let enemyIndex = chooseOpponent();
-
-    
-
-
-    let roll = diceRoll();
-
-    
-
-
-
-  }
 
 };
+
+
+  export class Battle {
+    constructor(user, opponent) {
+      this.user=user;
+      this.opponent=opponent;
+      this.roll = 0;
+      this.battleWon = false;
+    }
+    
+    diceRoll(){
+      let dicePair = [];
+      for (i = 0; i < 2; i++) {
+        let ranNumber = Math.floor(Math.random() * 6) + 1;
+        dicePair.push(diceNumber);
+      }
+      return dicePair;
+    }
+    doBattle(){
+      
+      // let enemyIndex = chooseOpponent();
+      // let name = this.characters[enemyIndex].name;
+      // let seniority = this.character[enemyIndex].seniority;
+      // let assets = this.character[enemyIndex].seniority;
+      let battleWon = false; 
+      let roll = diceRoll();
+      let diceRoll = roll[0] + roll[1];
+      // userPower takes seniority and then adds the modifiers from assets
+      let userPower = this.user.seniority + Object.values(this.user.assets[0]) + Object.values(this.user.assets[1]);
+      // margin is the equation for determining how much you have to roll to win
+      let margin = this.opponent.seniority - userPower + 5;
+      // if we modify user stats + opponent stats here, how do we return the outcome to the game?
+      // update character and opponent after battle
+
+      if (diceRoll > margin) {
+        battleWon = true;
+      }
+      else {
+        battleWon = false;
+      }
+    }
+  
+  };
+  
+    
+
+    // if opponent has 7 seniority and player has 3
+    // take opponents seniority, subtract your seniority, then add 5
+    // you have to roll that number or higher to win
+    // 7 - 3 = 4        4 + 5?? = 9
+    // must roll 9 or higher to win
+    
+
+  // }
 
 //let ranNumber = Math.floor(Math.random() * 6)+1;
